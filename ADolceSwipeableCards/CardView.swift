4f6905835d8e8fce5519d1dfd85124112af76401
Andrew Dolce <andrew@intrepid.io>
2016-01-21 19:58:27 -0500

    Add CardView subclass

---
+++ ADolceSwipeableCards/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  ADolceSwipeableCards
+//
+//  Created by Andrew Dolce on 1/21/16.
+//  Copyright © 2016 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
