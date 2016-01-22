b800cedb97ed21b37b1449aca71503017df68e85
Andrew Dolce <andrew@intrepid.io>
2016-01-21 19:58:38 -0500

    Position cards in center of main view

--- ADolceSwipeableCards/ViewController.swift
+++ ADolceSwipeableCards/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardContainerView: UIView!
+
+    private var cardColors = [UIColor]()
+    private var cardViews = [UIView]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        createTestCards()
+        createCardViews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func createTestCards() {
+        cardColors = [
+            UIColor.redColor(),
+            UIColor.orangeColor(),
+            UIColor.yellowColor(),
+            UIColor.greenColor(),
+            UIColor.blueColor(),
+            UIColor.purpleColor()
+        ]
     }
 
+    private func createCardViews() {
+        cardViews = cardColors.map { color in
+            let cardView = CardView(frame: self.cardContainerView.frame)
+            cardView.backgroundColor = color
 
+            self.cardContainerView.addSubview(cardView)
+
+            let margins = self.cardContainerView.layoutMarginsGuide
+            cardView.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
+            cardView.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
+            cardView.topAnchor.constraintEqualToAnchor(margins.topAnchor).active = true
+            cardView.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor).active = true
+
+            cardView.translatesAutoresizingMaskIntoConstraints = false
+
+            return cardView
+        }
+    }
 }
 
