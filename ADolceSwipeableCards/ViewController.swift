43c0e7d8bae670267b6273b692097414a414bb8d
Andrew Dolce <andrew@intrepid.io>
2016-02-02 22:56:05 -0500

    Revise card stack setup

--- ADolceSwipeableCards/ViewController.swift
+++ ADolceSwipeableCards/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     private var cardViewsInStack = [UIView]()
     private var flyingCardView: UIView? = nil
 
+    private var currentIndex: Int = 0
+    private var numberOfCardsToShow: Int {
+        return min(cards.count, 3)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,67 +41,83 @@ class ViewController: UIViewController {
     }
 
     private func createCardViews() {
-        cardViewsInStack = cards.map { card in
-            let cardView = CardView(frame: self.cardContainerView.frame)
-            cardView.backgroundColor = card.color
+        currentIndex = 0
 
-            self.cardContainerView.addSubview(cardView)
-
-            let margins = self.cardContainerView.layoutMarginsGuide
-            cardView.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
-            cardView.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
-            cardView.topAnchor.constraintEqualToAnchor(margins.topAnchor).active = true
-            cardView.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor).active = true
-
-            cardView.translatesAutoresizingMaskIntoConstraints = false
-
-            return cardView
+        for index in 0..<numberOfCardsToShow {
+            let cardView = cardViewForIndex(index)
+            addCardViewToBackOfStack(cardView)
         }
+
+        setTransformsOnCardsInStack(cardViewsInStack)
     }
 
-    private func updateCardViews() {
-        let hiddenCardTransform = CGAffineTransformMakeScale(0.8, 0.8);
+    private func cardViewForIndex(index: Int) -> UIView {
+        let card = cards[index];
+        let cardView = CardView(frame: cardContainerView.frame)
+        cardView.backgroundColor = card.color
+        return cardView
+    }
 
+    private func addCardViewToBackOfStack(cardView: UIView) {
+        cardViewsInStack.append(cardView)
+        self.cardContainerView.insertSubview(cardView, atIndex: 0)
+
+        let margins = self.cardContainerView.layoutMarginsGuide
+        cardView.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
+        cardView.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
+        cardView.topAnchor.constraintEqualToAnchor(margins.topAnchor).active = true
+        cardView.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor).active = true
+
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private func setTransformsOnCardsInStack(cardViews: [UIView]) {
         let transformsFrontToBack = [
             CGAffineTransformIdentity,
             CGAffineTransformTranslate(CGAffineTransformMakeScale(0.9, 0.9), 0, -40),
-            CGAffineTransformTranslate(CGAffineTransformMakeScale(0.8, 0.8), 0, -80)
+            CGAffineTransformTranslate(CGAffineTransformMakeScale(0.8, 0.8), 0, -80),
         ]
 
-        for (index, cardView) in cardViewsInStack.reverse().enumerate() {
-            if index < transformsFrontToBack.count {
-                cardView.transform = transformsFrontToBack[index]
-                cardView.alpha = 1
-            } else {
-                cardView.transform = hiddenCardTransform
-                cardView.alpha = 0
-            }
+        for (index, cardView) in cardViews.enumerate() {
+            cardView.transform = transformsFrontToBack[index]
         }
     }
 
-    private func moveFrontCardToBack() {
-        guard let frontCard = cards.last, let frontCardView = cardViewsInStack.last else {
+    private func animateToNextCard() {
+        guard let frontCardView = cardViewsInStack.first else {
             return
         }
 
-        cards.popLast()
-        cards.insert(frontCard, atIndex: 0)
-        cardViewsInStack.popLast()
-
         throwCardView(frontCardView)
 
-        UIView.animateWithDuration(0.5, animations: updateCardViews)
+        // Add the next card
+        let newIndex = (currentIndex + numberOfCardsToShow) % cards.count
+        let newCardView = cardViewForIndex(newIndex)
+        addCardViewToBackOfStack(newCardView)
+
+        // Advance the current index
+        currentIndex += 1
+
+        // Animate and update the stack
+        newCardView.transform = CGAffineTransformMakeScale(0.8, 0.8)
+
+
+        let newStack = Array(cardViewsInStack[1..<cardViewsInStack.count])
+
+        UIView.animateWithDuration(0.5, animations: {
+            // Animate cards forward in stack
+            self.setTransformsOnCardsInStack(newStack)
+        }, completion: { finished in
+            frontCardView.removeFromSuperview()
+            self.cardViewsInStack = newStack
+        })
     }
 
     private func throwCardView(cardView: UIView) {
         let thrownTransform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(CGFloat(M_1_PI) / 4.0), 400, 0)
-        UIView.animateWithDuration(0.5, animations: {
+        UIView.animateWithDuration(0.5) {
             cardView.transform = thrownTransform
-        }, completion: { finished in
-            cardView.alpha = 0
-            self.cardContainerView.sendSubviewToBack(cardView)
-            self.cardViewsInStack.insert(cardView, atIndex: 0)
-        })
+        }
     }
 
     // MARK: Gestures
@@ -107,7 +128,7 @@ class ViewController: UIViewController {
     }
 
     dynamic private func didTapCards(recognizer: UITapGestureRecognizer) {
-        moveFrontCardToBack()
+        animateToNextCard()
     }
 }
 
